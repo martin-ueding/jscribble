961e661965004aaefa135de79f3ad51b996ccc25
Martin Ueding <dev@martin-ueding.de>
2011-06-26 23:54:10 +0200

    documented tests, removed UUID from tempfile name

--- tests/jscribble/notebook/NoteSheetTest.java
+++ tests/jscribble/notebook/NoteSheetTest.java
@@ -5,7 +5,6 @@ package tests.jscribble.notebook;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 import jscribble.notebook.NoteSheet;
 import junit.framework.TestCase;
@@ -13,6 +12,9 @@ import junit.framework.TestCase;
 
 public class NoteSheetTest extends TestCase {
 
+	/**
+	 * Generates a temporary NoteSheet for testing.
+	 */
 	private NoteSheet getTempNoteSheet() {
 		return new NoteSheet(new Dimension(1024, 600), 0, null);
 	}
@@ -35,6 +37,9 @@ public class NoteSheetTest extends TestCase {
 		assertFalse(rgbarray[0] == previousColor);
 	}
 
+	/**
+	 * Tests whether a new NoteSheet is untouched and does not need any saving.
+	 */
 	public void testTouched() {
 		NoteSheet n = getTempNoteSheet();
 		assertFalse(n.touched());
@@ -44,9 +49,12 @@ public class NoteSheetTest extends TestCase {
 		assertTrue(n.unsaved());
 	}
 
+	/**
+	 * Creates a single NoteSheet with an existing temporary file and tests whether it is untouched and does not need any saving.
+	 */
 	public void testTouchedWithEmptyTempfile() {
 		try {
-			File tempfile = File.createTempFile(UUID.randomUUID().toString(), "");
+			File tempfile = File.createTempFile("", ".png");
 			tempfile.createNewFile();
 			NoteSheet n = new NoteSheet(new Dimension(100, 100), 0, tempfile);
 
