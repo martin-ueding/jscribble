09c8dacfe14deea5703e7f18f740154e01340855
Martin Ueding <dev@martin-ueding.de>
2011-06-29 10:32:43 +0200

    added VersionNameTest

---
+++ tests/jscribble/VersionNameTest.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de>
+
+package tests.jscribble;
+
+import java.util.regex.Pattern;
+
+import jscribble.VersionName;
+import junit.framework.TestCase;
+
+public class VersionNameTest extends TestCase {
+	public VersionNameTest() {
+		super();
+	}
+	
+	public void testVersionName() {
+		Pattern p = Pattern.compile("\\d+(\\.\\d+)+~\\d+");
+		assertTrue(p.matcher(VersionName.version).matches());
+	}
+
+}
