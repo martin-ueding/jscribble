1c0842f30855f4472b3212176a6eb49eeef4624b
Martin Ueding <dev@martin-ueding.de>
2011-06-29 10:37:31 +0200

    added doc

--- tests/jscribble/VersionNameTest.java
+++ tests/jscribble/VersionNameTest.java
@@ -7,11 +7,20 @@ import java.util.regex.Pattern;
 import jscribble.VersionName;
 import junit.framework.TestCase;
 
+/**
+ * Tests the VersionName class.
+ *
+ * @author Martin Ueding <dev@martin-ueding.de>
+ */
 public class VersionNameTest extends TestCase {
 	public VersionNameTest() {
 		super();
 	}
 
+
+	/**
+	 * Checks whether the version name is in the correct format x.xx~xxx.
+	 */
 	public void testVersionName() {
 		Pattern p = Pattern.compile("\\d+(\\.\\d+)+~\\d+");
 		assertTrue(p.matcher(VersionName.version).matches());
