7974ca09d6df0ed75c5ce1fe3e8c717b029a8aa9
Martin Ueding <dev@martin-ueding.de>
2011-08-06 22:32:22 +0200

    added -v flag to activate log messages, astyle

--- jscribble/NoteBookProgram.java
+++ jscribble/NoteBookProgram.java
@@ -2,17 +2,17 @@
 
 /*
  * This file is part of jscribble.
- * 
+ *
  * Foobar is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * jscribble is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * jscribble.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -85,19 +85,23 @@ public class NoteBookProgram {
 	 * @param message message
 	 */
 	public static void log(String reportingClass, String message) {
-		Calendar c = Calendar.getInstance();
-		String date = String.format("%d-%02d-%02d %02d:%02d:%02d.%03d",
-		        c.get(Calendar.YEAR),
-		        c.get(Calendar.MONTH),
-		        c.get(Calendar.DAY_OF_MONTH),
-		        c.get(Calendar.HOUR_OF_DAY),
-		        c.get(Calendar.MINUTE),
-		        c.get(Calendar.SECOND),
-		        c.get(Calendar.MILLISECOND)
-		                           );
-		String output =  date + " " + reportingClass + ":\t" + message;
-		System.out.println(output);
+		if (debug) {
+			Calendar c = Calendar.getInstance();
+			String date = String.format("%d-%02d-%02d %02d:%02d:%02d.%03d",
+			        c.get(Calendar.YEAR),
+			        c.get(Calendar.MONTH),
+			        c.get(Calendar.DAY_OF_MONTH),
+			        c.get(Calendar.HOUR_OF_DAY),
+			        c.get(Calendar.MINUTE),
+			        c.get(Calendar.SECOND),
+			        c.get(Calendar.MILLISECOND)
+			                           );
+			String output =  date + " " + reportingClass + ":\t" + message;
+			System.out.println(output);
+		}
 	}
+
+	private static boolean debug = false;
 
 
 	/**
@@ -106,6 +110,14 @@ public class NoteBookProgram {
 	 * @param args CLI arguments
 	 */
 	public static void main(String[] args) {
+		for (String string : args) {
+			if (string.equalsIgnoreCase("-v")) {
+				debug = true;
+			}
+		}
+
+
+
 		printVersionIfNeeded(args);
 
 		log(getProgramname(), Localizer.get("Starting up."));
