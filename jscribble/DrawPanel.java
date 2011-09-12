663970364bc03af2573365f12cc8c74478a3b32a
Martin Ueding <dev@martin-ueding.de>
2011-09-12 20:27:38 +0200

    80 cols

--- jscribble/DrawPanel.java
+++ jscribble/DrawPanel.java
@@ -3,10 +3,10 @@
 /*
  * This file is part of jscribble.
  *
- * jscribble is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 2 of the License, or (at your option) any later
- * version.
+ * jscribble is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
  *
  * jscribble is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -225,7 +225,8 @@ public class DrawPanel extends JPanel {
 		if (onionMode) {
 			BufferedImage prev = notebook.getPreviousSheet().getImg();
 			if (prev != null) {
-				g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, (float) 0.5));
+				g2.setComposite(AlphaComposite.getInstance(
+				            AlphaComposite.SRC_ATOP, (float) 0.5));
 				g2.drawImage(prev, 0, 0, io);
 			}
 		}
