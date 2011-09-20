87497de0a036887642ea679d8729df2c8002bc7d
Martin Ueding <dev@martin-ueding.de>
2011-09-20 16:18:11 +0200

    do not create a default config

--- jscribble/SettingsWrapper.java
+++ jscribble/SettingsWrapper.java
@@ -1,12 +1,9 @@
 package jscribble;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class SettingsWrapper {
@@ -29,25 +26,7 @@ public class SettingsWrapper {
 				        "config.txt");
 				if (configfile.exists()) {
 					mainConfig.load(new FileInputStream(configfile));
-				}
-				// Create a new config file if there is none, fill it with the
-				// defaults from the distribution.
-				else {
-					NoteBookProgram.log(NoteBookProgram.class.getClass().getName(),
-					        Localizer.get("Could not find config file. Writing a default one."));
-					InputStreamReader isr = new InputStreamReader(
-					    ClassLoader.getSystemClassLoader()
-					    .getResourceAsStream("jscribble/config.txt"));
-					BufferedReader br = new BufferedReader(isr);
-					FileWriter fw = new FileWriter(configfile);
-					String line;
-					while ((line = br.readLine()) != null) {
-						fw.write(line + "\n");
-					}
-					br.close();
-					isr.close();
-					fw.close();
-				}
+			}
 			}
 			catch (FileNotFoundException e) {
 				NoteBookProgram.handleError(Localizer.get("Could not find the config file. (This should *not* happen!)"));
