1b8079a4b5f2d8b33d7c4519b0ca25e9fa8c3188
Martin Ueding <dev@martin-ueding.de>
2011-09-20 16:53:42 +0200

    add copyright notice, sort members, add javadoc

--- jscribble/Logger.java
+++ jscribble/Logger.java
@@ -1,10 +1,40 @@
+// Copyright © 2011 Martin Ueding <dev@martin-ueding.de>
+
+/*
+ * This file is part of jscribble.
+ *
+ * jscribble is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * jscribble is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * jscribble.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package jscribble;
 
 import java.util.Calendar;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Handles logging and error reporting
+ *
+ * @author Martin Ueding <dev@martin-ueding.de>
+ */
 public class Logger {
+	/**
+	 * Whether to show debug information by default. This can be overridden by
+	 * the "-v" command line option.
+	 */
+	private static boolean debug = false;
+
 	/**
 	 * Handles some error message centrally, right now it just displays a
 	 * dialog box with the error message.
@@ -16,14 +46,6 @@ public class Logger {
 		JOptionPane.showMessageDialog(null, errorMessage);
 		System.exit(1);
 	}
-
-
-
-	/**
-	 * Whether to show debug information by default. This can be overridden by
-	 * the "-v" command line option.
-	 */
-	private static boolean debug = false;
 
 	/**
 	 * Writes a message to a log file.
