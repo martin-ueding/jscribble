7cd431d0b75d5b1989ba65f0ce86edd44a49967b
Martin Ueding <dev@martin-ueding.de>
2011-10-07 15:47:03 +0200

    Sort fiels in config.js

--- config/config.js
+++ config/config.js
@@ -1,326 +1,326 @@
 [
 {
-	"type": "String",
-	"comment": "Date format used for logging.",
 	"key": "date_format",
-	"value": "yyyy-MM-dd HH:mm:ss"
+	"value": "yyyy-MM-dd HH:mm:ss",
+	"type": "String",
+	"comment": "Date format used for logging."
 },
 {
-	"type": "Color",
-	"comment": "Background color of the online help screen. This is a translucent black currently.",
 	"key": "help_screen_background_color",
-	"value": "C8000000"
+	"value": "C8000000",
+	"type": "Color",
+	"comment": "Background color of the online help screen. This is a translucent black currently."
 },
 {
-	"type": "Integer",
-	"comment": "Border radius on the backdrop of the online help screen.",
 	"key": "help_screen_border_radius",
-	"value": "20"
+	"value": "20",
+	"type": "Integer",
+	"comment": "Border radius on the backdrop of the online help screen."
 },
 {
-	"type": "Key",
-	"comment": "Closes the online help screen.",
 	"key": "help_screen_close_key",
-	"value": "27"
+	"value": "27",
+	"type": "Key",
+	"comment": "Closes the online help screen."
 },
 {
-	"type": "Integer",
-	"comment": "Margin outside the backdrop.",
 	"key": "help_screen_margin",
-	"value": "50"
+	"value": "50",
+	"type": "Integer",
+	"comment": "Margin outside the backdrop."
 },
 {
-	"type": "Integer",
-	"comment": "Padding inside the backdrop.",
 	"key": "help_screen_padding",
-	"value": "70"
+	"value": "70",
+	"type": "Integer",
+	"comment": "Padding inside the backdrop."
 },
 {
-	"type": "Integer",
-	"comment": "Spacing between left and right column of the help screen.",
 	"key": "help_screen_spacing",
-	"value": "250"
+	"value": "250",
+	"type": "Integer",
+	"comment": "Spacing between left and right column of the help screen."
 },
 {
-	"type": "Key",
-	"comment": "Toggles the online help screen.",
 	"key": "help_screen_toggle_key",
-	"value": "h,112"
+	"value": "h,112",
+	"type": "Key",
+	"comment": "Toggles the online help screen."
 },
 {
-	"type": "Integer",
-	"comment": "Spacing between the rows of the help screen.",
 	"key": "help_screen_vspacing",
-	"value": "30"
+	"value": "30",
+	"type": "Integer",
+	"comment": "Spacing between the rows of the help screen."
 },
 {
-	"type": "Color",
-	"comment": "Color of the help splash. This is a very translucent black currently.",
 	"key": "help_splash_background_color",
-	"value": "64000000"
+	"value": "64000000",
+	"type": "Color",
+	"comment": "Color of the help splash. This is a very translucent black currently."
 },
 {
-	"type": "Integer",
-	"comment": "Corner radius of the help splash screen.",
 	"key": "help_splash_border_radius",
-	"value": "20"
+	"value": "20",
+	"type": "Integer",
+	"comment": "Corner radius of the help splash screen."
 },
 {
-	"type": "Boolean",
-	"comment": "Enables the help splash at startup.",
 	"key": "help_splash_enable",
-	"value": "true"
+	"value": "true",
+	"type": "Boolean",
+	"comment": "Enables the help splash at startup."
 },
 {
-	"type": "Integer",
-	"comment": "Height of the help splash box.",
 	"key": "help_splash_height",
-	"value": "50"
+	"value": "50",
+	"type": "Integer",
+	"comment": "Height of the help splash box."
 },
 {
-	"type": "Integer",
-	"comment": "Margin around the help splash.",
 	"key": "help_splash_margin",
-	"value": "15"
+	"value": "15",
+	"type": "Integer",
+	"comment": "Margin around the help splash."
 },
 {
-	"type": "Integer",
-	"comment": "Distance of the memory usage from the bottom.",
 	"key": "memory_usage_position_bottom",
-	"value": "10"
+	"value": "10",
+	"type": "Integer",
+	"comment": "Distance of the memory usage from the bottom."
 },
 {
-	"type": "Integer",
-	"comment": "Distance of the memory usage from the left.",
 	"key": "memory_usage_position_left",
-	"value": "10"
+	"value": "10",
+	"type": "Integer",
+	"comment": "Distance of the memory usage from the left."
 },
 {
-	"type": "Boolean",
-	"comment": "Enables the memory usage display.",
 	"key": "memory_usage_show",
-	"value": "false"
+	"value": "false",
+	"type": "Boolean",
+	"comment": "Enables the memory usage display."
 },
 {
-	"type": "Color",
-	"comment": "Color of the paper.",
 	"key": "notebook_background_color",
-	"value": "FFFFFF"
+	"value": "FFFFFF",
+	"type": "Color",
+	"comment": "Color of the paper."
 },
 {
-	"type": "Integer",
-	"comment": "Number of notesheets that are to be cached when moving back and forward in the notebook.",
 	"key": "notebook_cache_width",
-	"value": "10"
+	"value": "10",
+	"type": "Integer",
+	"comment": "Number of notesheets that are to be cached when moving back and forward in the notebook."
 },
 {
-	"type": "Key",
-	"comment": "Closes the current notebook.",
 	"key": "notebook_close_key",
-	"value": "q"
+	"value": "q",
+	"type": "Key",
+	"comment": "Closes the current notebook."
 },
 {
-	"type": "Integer",
-	"comment": "Height of new notebooks if the user does not use his native resolution.",
 	"key": "notebook_default_height",
-	"value": "600"
+	"value": "600",
+	"type": "Integer",
+	"comment": "Height of new notebooks if the user does not use his native resolution."
 },
 {
-	"type": "Integer",
-	"comment": "Width of new notebooks if the user does not use his native resolution",
 	"key": "notebook_default_width",
-	"value": "1024"
+	"value": "1024",
+	"type": "Integer",
+	"comment": "Width of new notebooks if the user does not use his native resolution"
 },
 {
-	"type": "Mouse",
-	"comment": "Mouse button to use for drawing.",
 	"key": "notebook_draw_mouse_button",
-	"value": "0,1024"
+	"value": "0,1024",
+	"type": "Mouse",
+	"comment": "Mouse button to use for drawing."
 },
 {
-	"type": "Integer",
-	"comment": "Width of the pen.",
 	"key": "notebook_draw_stroke_width",
-	"value": "1"
+	"value": "1",
+	"type": "Integer",
+	"comment": "Width of the pen."
 },
 {
-	"type": "Boolean",
-	"comment": "Enables the eraser.",
 	"key": "notebook_erase_enable",
-	"value": "true"
-},
-{
-	"type": "Mouse",
-	"comment": "Mouse button to use for erasing.",
-	"key": "notebook_erase_mouse_button",
-	"value": "256,4096"
-},
-{
-	"type": "Integer",
-	"comment": "Width of the eraser. It should be way larger than the pen to make erasing easy.",
-	"key": "notebook_erase_stroke_width",
-	"value": "8"
-},
-{
-	"type": "Integer",
-	"comment": "Time in milliseconds to wait after last erasing to repaint the onion layers and ruling.",
-	"key": "notebook_erase_timeout",
-	"value": "100"
-},
-{
-	"type": "Color",
-	"comment": "Color of the pen.",
-	"key": "notebook_foreground_color",
-	"value": "000000"
-},
-{
-	"type": "Key",
-	"comment": "Key to go back a page.",
-	"key": "notebook_go_back_key",
-	"value": "k,38,37,08"
-},
-{
-	"type": "Mouse",
-	"comment": "Mouse button to go back a page.",
-	"key": "notebook_go_back_mouse_button",
-	"value": ""
-},
-{
-	"type": "Key",
-	"comment": "Key to go forward a page.",
-	"key": "notebook_go_forward_key",
-	"value": "j,40,39,32,10"
-},
-{
-	"type": "Mouse",
-	"comment": "Mouse button to go forward a page.",
-	"key": "notebook_go_forward_mouse_button",
-	"value": ""
-},
-{
-	"type": "Key",
-	"comment": "Key to go to first page.",
-	"key": "notebook_goto_first_key",
-	"value": "f,36"
-},
-{
-	"type": "Key",
-	"comment": "Key to go to last page.",
-	"key": "notebook_goto_last_key",
-	"value": "l,35"
-},
-{
-	"type": "String",
-	"comment": "The name of every new notebook is validated against this. This prevents anything cumbersome in file names.",
-	"key": "notebook_name_validation_pattern",
-	"value": "[A-Za-z0-9-_]+"
-},
-{
-	"type": "Integer",
-	"comment": "Height of the initial selection window.",
-	"key": "notebook_selection_window_height",
-	"value": "300"
-},
-{
-	"type": "Integer",
-	"comment": "Width of the initial selection window.",
-	"key": "notebook_selection_window_width",
-	"value": "400"
-},
-{
-	"type": "Integer",
-	"comment": "Position of the onion layer info from the left.",
-	"key": "onion_info_position_left",
-	"value": "10"
-},
-{
-	"type": "Integer",
-	"comment": "Position of the onion layer info from the top.",
-	"key": "onion_info_position_top",
-	"value": "15"
-},
-{
-	"type": "Key",
-	"comment": "Removes one onion layer.",
-	"key": "onion_layer_decrease_key",
-	"value": "-"
-},
-{
-	"type": "Key",
-	"comment": "Adds one onion layer.",
-	"key": "onion_layer_increase_key",
-	"value": "+"
-},
-{
-	"type": "Float",
-	"comment": "Opacity of the individual onion layers. If this is set high, one can see the other layers pretty well, if one sets it low, a better separation is achieved.",
-	"key": "onion_mode_opacity",
-	"value": "0.8"
-},
-{
-	"type": "Integer",
-	"comment": "Position of the page number from the top.",
-	"key": "page_number_position_top",
-	"value": "15"
-},
-{
-	"type": "String",
-	"comment": "The name is used for the settings directory. If this is changed, all previously made notebooks are still on the disk, but cannot be accessed through the program any more.",
-	"key": "program_name",
-	"value": "jscribble"
-},
-{
-	"type": "Key",
-	"comment": "Key to toggle graph ruling.",
-	"key": "ruling_graph_toggle_key",
-	"value": "g"
-},
-{
-	"type": "Color",
-	"comment": "Color of the ruling. It is drawn with the same opaqueness as the onion layers.",
-	"key": "ruling_line_color",
-	"value": "646464"
-},
-{
-	"type": "Integer",
-	"comment": "Spacing between the lines or squares.",
-	"key": "ruling_line_spacing",
-	"value": "40"
-},
-{
-	"type": "Key",
-	"comment": "Key to toggle line ruling.",
-	"key": "ruling_toggle_key",
-	"value": "r"
-},
-{
-	"type": "Color",
-	"comment": "Color of the mouse only mode scroll panels.",
-	"key": "scroll_panel_color",
-	"value": "64000000"
-},
-{
-	"type": "Integer",
-	"comment": "Padding top and bottom of the scroll panels.",
-	"key": "scroll_panel_padding",
-	"value": "5"
-},
-{
-	"type": "Integer",
-	"comment": "Width of the scroll panels.",
-	"key": "scroll_panel_width",
-	"value": "20"
-},
-{
+	"value": "true",
 	"type": "Boolean",
-	"comment": "Enables panels at the side of the screen to navigate without a keyboard.  Useful for tablet computers.",
-	"key": "scroll_panels_show",
-	"value": "false"
+	"comment": "Enables the eraser."
 },
 {
+	"key": "notebook_erase_mouse_button",
+	"value": "256,4096",
+	"type": "Mouse",
+	"comment": "Mouse button to use for erasing."
+},
+{
+	"key": "notebook_erase_stroke_width",
+	"value": "8",
+	"type": "Integer",
+	"comment": "Width of the eraser. It should be way larger than the pen to make erasing easy."
+},
+{
+	"key": "notebook_erase_timeout",
+	"value": "100",
+	"type": "Integer",
+	"comment": "Time in milliseconds to wait after last erasing to repaint the onion layers and ruling."
+},
+{
+	"key": "notebook_foreground_color",
+	"value": "000000",
+	"type": "Color",
+	"comment": "Color of the pen."
+},
+{
+	"key": "notebook_go_back_key",
+	"value": "k,38,37,08",
+	"type": "Key",
+	"comment": "Key to go back a page."
+},
+{
+	"key": "notebook_go_back_mouse_button",
+	"value": "",
+	"type": "Mouse",
+	"comment": "Mouse button to go back a page."
+},
+{
+	"key": "notebook_go_forward_key",
+	"value": "j,40,39,32,10",
+	"type": "Key",
+	"comment": "Key to go forward a page."
+},
+{
+	"key": "notebook_go_forward_mouse_button",
+	"value": "",
+	"type": "Mouse",
+	"comment": "Mouse button to go forward a page."
+},
+{
+	"key": "notebook_goto_first_key",
+	"value": "f,36",
+	"type": "Key",
+	"comment": "Key to go to first page."
+},
+{
+	"key": "notebook_goto_last_key",
+	"value": "l,35",
+	"type": "Key",
+	"comment": "Key to go to last page."
+},
+{
+	"key": "notebook_name_validation_pattern",
+	"value": "[A-Za-z0-9-_]+",
 	"type": "String",
-	"comment": "It does not make much sense to overwrite this since this property is always looked up in the default config.",
+	"comment": "The name of every new notebook is validated against this. This prevents anything cumbersome in file names."
+},
+{
+	"key": "notebook_selection_window_height",
+	"value": "300",
+	"type": "Integer",
+	"comment": "Height of the initial selection window."
+},
+{
+	"key": "notebook_selection_window_width",
+	"value": "400",
+	"type": "Integer",
+	"comment": "Width of the initial selection window."
+},
+{
+	"key": "onion_info_position_left",
+	"value": "10",
+	"type": "Integer",
+	"comment": "Position of the onion layer info from the left."
+},
+{
+	"key": "onion_info_position_top",
+	"value": "15",
+	"type": "Integer",
+	"comment": "Position of the onion layer info from the top."
+},
+{
+	"key": "onion_layer_decrease_key",
+	"value": "-",
+	"type": "Key",
+	"comment": "Removes one onion layer."
+},
+{
+	"key": "onion_layer_increase_key",
+	"value": "+",
+	"type": "Key",
+	"comment": "Adds one onion layer."
+},
+{
+	"key": "onion_mode_opacity",
+	"value": "0.8",
+	"type": "Float",
+	"comment": "Opacity of the individual onion layers. If this is set high, one can see the other layers pretty well, if one sets it low, a better separation is achieved."
+},
+{
+	"key": "page_number_position_top",
+	"value": "15",
+	"type": "Integer",
+	"comment": "Position of the page number from the top."
+},
+{
+	"key": "program_name",
+	"value": "jscribble",
+	"type": "String",
+	"comment": "The name is used for the settings directory. If this is changed, all previously made notebooks are still on the disk, but cannot be accessed through the program any more."
+},
+{
+	"key": "ruling_graph_toggle_key",
+	"value": "g",
+	"type": "Key",
+	"comment": "Key to toggle graph ruling."
+},
+{
+	"key": "ruling_line_color",
+	"value": "646464",
+	"type": "Color",
+	"comment": "Color of the ruling. It is drawn with the same opaqueness as the onion layers."
+},
+{
+	"key": "ruling_line_spacing",
+	"value": "40",
+	"type": "Integer",
+	"comment": "Spacing between the lines or squares."
+},
+{
+	"key": "ruling_toggle_key",
+	"value": "r",
+	"type": "Key",
+	"comment": "Key to toggle line ruling."
+},
+{
+	"key": "scroll_panel_color",
+	"value": "64000000",
+	"type": "Color",
+	"comment": "Color of the mouse only mode scroll panels."
+},
+{
+	"key": "scroll_panel_padding",
+	"value": "5",
+	"type": "Integer",
+	"comment": "Padding top and bottom of the scroll panels."
+},
+{
+	"key": "scroll_panel_width",
+	"value": "20",
+	"type": "Integer",
+	"comment": "Width of the scroll panels."
+},
+{
+	"key": "scroll_panels_show",
+	"value": "false",
+	"type": "Boolean",
+	"comment": "Enables panels at the side of the screen to navigate without a keyboard.  Useful for tablet computers."
+},
+{
 	"key": "user_config_filename",
-	"value": "config.properties"
+	"value": "config.properties",
+	"type": "String",
+	"comment": "It does not make much sense to overwrite this since this property is always looked up in the default config."
 }
 ]
