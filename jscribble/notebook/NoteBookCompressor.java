7920ef94e2289c40050c4e45b0b66880f05e8729
Martin Ueding <dev@martin-ueding.de>
2011-11-14 13:43:15 +0100

    Add compressor class

---
+++ jscribble/notebook/NoteBookCompressor.java
@@ -0,0 +1,29 @@
+// Copyright © 2011 Martin Ueding <dev@martin-ueding.de>
+
+/*
+ * This file is part of jscribble.
+ *
+ * jscribble is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * jscribble is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * jscribble.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jscribble.notebook;
+
+/**
+ * Takes a folder which contains a notebook and compresses the file names so
+ * that there are no gaps in the numbering.
+ * 
+ * @author Martin Ueding <dev@martin-ueding.de>
+ */
+public class NoteBookCompressor {
+}
